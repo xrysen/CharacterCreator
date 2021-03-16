493ae0c9c737c29196b254f2c0c5fee35fcc8701
Sean Oyler <xrysen@gmail.com>
2021-03-16 13:25:16 -0700

    Adds subrace model and migrations

--- character/admin.py
+++ character/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Race, SubRace
+
+admin.site.register(Race)
+admin.site.register(SubRace)
--- character/models.py
+++ character/models.py
@@ -16,3 +16,18 @@ class Race(models.Model):
 
   def __str__(self):
     return self.race_name
+
+class SubRace(models.Model):
+  sub_race_name = models.CharField(max_length=50)
+  sub_race_parent = models.ForeignKey(Race, on_delete=models.CASCADE)
+  sub_race_description = models.TextField()
+  sub_race_str_bonus = models.IntegerField()
+  sub_race_dex_bonus = models.IntegerField()
+  sub_race_con_bonus = models.IntegerField()
+  sub_race_int_bonus = models.IntegerField()
+  sub_race_wis_bonus = models.IntegerField()
+  sub_race_cha_bonus = models.IntegerField()
+  sub_race_spd = models.IntegerField()
+
+  def __str__(self):
+    return self.sub_race_name
