71761f60714b64c488311ecc8a65e2760eda6fad
Sean Oyler <xrysen@gmail.com>
2021-03-17 09:09:52 -0700

    Shows correct sub race when choosing race

--- character/views.py
+++ character/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Race
+from .models import Race, SubRace
 
 # Create your views here.
 def index(request):
   all_races = Race.objects.all()
+  sub_races = SubRace.objects.all()
   context = {
-    'races': all_races
+    'races': all_races,
+    'sub_races': sub_races
   }
 
   return render(request, 'index.html', context)
