4e0759b49cbe4d31447a8f85b4c9f66c51e3b59e
Sean Oyler <xrysen@gmail.com>
2021-03-17 12:28:03 -0700

    Shows and hides descriptions for each race and sub-race

--- character/static/js/scripts.js
+++ character/static/js/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(() => {
   console.log("Connected");
 
-  for (let i = 0; i < 12; i++) {
-    $(`#${i}`).css("display", "none");
+  const hideAll = () => {
+    for (let i = 0; i < 12; i++) {
+      $(`#rdescription-${i}`).css("display", "none");
+      $(`#srdescription-${i}`).css("display", "none");
+    }
   }
+
+  hideAll();
+
+  for (let i = 0; i < 12; i++) {
+    $(`#race-${i}`).on("click", ()=> {
+      hideAll();
+      $(`#rdescription-${i}`).css("display", "block");
+    })
+  }
+
+  for (let i = 0; i < 12; i++) {
+    $(`#subrace-${i}`).on("click", ()=> {
+      $(`#srdescription-${i}`).css("display","block");
+    })
+  }
+
 
 });
