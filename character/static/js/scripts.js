e0d130d23ad530dbffa189766355311dbd2ba73e
Sean Oyler <xrysen@gmail.com>
2021-03-18 15:34:15 -0700

    Sets up 2 phase of character creation

--- character/static/js/scripts.js
+++ character/static/js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(() => {
+  let race_choice = 0;
+  let sub_race_choice = 0;
+  let progress = "";
+
   console.log("Connected");
 
   const hideAll = () => {
@@ -10,29 +14,56 @@ $(document).ready(() => {
     }
   }
 
-  hideAll();
+  const emptyMainContainer = () => {
+    $(".race-container").remove();
+  }
 
-  for (let i = 0; i < 12; i++) {
-    $(`#race-${i}`).on("click", ()=> {
-      hideAll();
-      $(`#r-header-${i}`).css("display", "block");
-      $(`#rdescription-${i}`).css("display", "block");
-      if (i !== 2 && i !== 3 && i !== 4 && i !== 6 ) {
-        $(`.main-container`).append(
-          `<button class = "btn btn-primary" id = "next-btn">Next</button>`
+  const next = (progress) => {
+    switch (progress) {
+      case "class":
+        emptyMainContainer();
+    }
+  }
+
+  const chooseRace = () => {
+    for (let i = 0; i < 12; i++) {
+      $(`#race-${i}`).on("click", ()=> {
+        hideAll();
+        race_choice = i;
+        $(`#r-header-${i}`).css("display", "block");
+        $(`#rdescription-${i}`).css("display", "block");
+        if (i !== 2 && i !== 3 && i !== 4 && i !== 6 ) {
+          $(`.race-container`).append(
+            `<button class = "btn btn-primary" id = "next-btn" style="margin-bottom: 20px;">Next</button>`
+          );
+
+          $("#next-btn").on("click", ()=> {
+            next("class");
+          });
+        }
+      })
+    }
+  
+    for (let i = 0; i < 12; i++) {
+      $(`#subrace-${i}`).on("click", ()=> {
+        sub_race_choice = i;
+        $(`#srdescription-${i}`).css("display","block");
+        $(`.race-container`).append(
+          `<button class = "btn btn-primary" id = "next-btn" style="margin-bottom: 20px;">Next</button>`
         );
-      }
-    })
+        $("#next-btn").on("click", ()=> {
+            next("class");
+          });
+      })
+    }
   }
 
-  for (let i = 0; i < 12; i++) {
-    $(`#subrace-${i}`).on("click", ()=> {
-      $(`#srdescription-${i}`).css("display","block");
-      $(`.main-container`).append(
-        `<button class = "btn btn-primary" id = "next-btn" style="margin-bottom: 20px;">Next</button>`
-      );
-    })
+  const chooseClass = (race) => {
+
   }
+
+  hideAll();
+  chooseRace();
 
 
 });
