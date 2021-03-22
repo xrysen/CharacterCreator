7cfbd5ee604eb5cccaa3b4d825f58789cd91424a
Sean Oyler <xrysen@gmail.com>
2021-03-22 14:44:54 -0700

    Sets up api filtering

--- character/urls.py
+++ character/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import RaceApiView, ClassApiView, SubRaceApiView
+from .views import RaceApiView, ClassApiView, SubRaceApiView, RaceList
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('race/', RaceApiView.as_view()),
+  path('race/', RaceList.as_view()),
   path('class/', ClassApiView.as_view()),
   path('subrace/', SubRaceApiView.as_view()),
 ]
--- character/views.py
+++ character/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from .models import Race, SubRace, Class, Skill
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from .serializers import RaceSerializer, ClassSerializer, SubRaceSerializer, SkillSerializer
 
 # Create your views here.
@@ -38,3 +38,15 @@ class SubRaceApiView(APIView):
     sub_races = SubRace.objects.all().values()
     serializer = SubRaceSerializer(sub_races, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+class RaceList(generics.ListAPIView):
+  serializer_class = RaceSerializer
+
+  def get_queryset(self):
+
+    queryset = Race.objects.all()
+    race_name = self.request.query_params.get('race_name', None)
+    if race_name is not None:
+      queryset = queryset.filter(race_name=race_name)
+    
+    return queryset
