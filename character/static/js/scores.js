80e59078a82b2e531d16a28a2594f1d53807ff6c
Sean Oyler <xrysen@gmail.com>
2021-03-23 16:38:03 -0700

    Adds sub race stats in addition to main racial stats

--- character/static/js/scores.js
+++ character/static/js/scores.js
@@ -1,10 +1,17 @@
-raceSelected = "Dwarf";
+raceSelected = "Elf";
 selectedClass = "Paladin";
+subRaceSelected = 1;
 
 const scoreCalcTemplate = () => {
+  let race = "";
+  if (subRaceSelected) {
+    race = subRaceSelected;
+  } else {
+    race = raceSelected;
+  }
   $(".main-container").append(
     `
-      <h3>Roll Ability Scores for your ${raceSelected} ${selectedClass}</h3>
+      <h3>Roll Ability Scores for your ${race} ${selectedClass}</h3>
       <div class = "card-group" id = "stat-cards-1" style="margin-left: 5%; margin-right: 5%">
       </div>
       <div class = "card-group" id = "stat-cards-2" style="margin-left: 5%; margin-right: 5%">
@@ -12,17 +19,21 @@ const scoreCalcTemplate = () => {
     `
   );
 
-  statTable(1, "Strength", raceSelected, "race_str_bonus");
-  statTable(1, "Dexterity", raceSelected, "race_dex_bonus");
-  statTable(1, "Constitution", raceSelected, "race_con_bonus");
-  statTable(2, "Intelligence", raceSelected, "race_int_bonus");
-  statTable(2, "Wisdom", raceSelected, "race_wis_bonus");
-  statTable(2, "Charisma", raceSelected, "race_cha_bonus");
+  statTable(1, "Strength", raceSelected, "race_str_bonus", "sub_race_str_bonus");
+  statTable(1, "Dexterity", raceSelected, "race_dex_bonus", "sub_race_dex_bonus");
+  statTable(1, "Constitution", raceSelected, "race_con_bonus", "sub_race_con_bonus");
+  statTable(2, "Intelligence", raceSelected, "race_int_bonus", "sub_race_int_bonus");
+  statTable(2, "Wisdom", raceSelected, "race_wis_bonus", "sub_race_wis_bonus");
+  statTable(2, "Charisma", raceSelected, "race_cha_bonus", "sub_race_cha_bonus");
 };
 
-const statTable = (group, statName, race, statAbbr) => {
+const statTable = (group, statName, race, statAbbr, subStatAbbr) => {
   const raceData = getRaceDataByName(race);
   raceData.then((res) => {
+    let subRaceStat = 0;
+    if (res[0].race_has_subrace) {
+      subRaceStat = res[0].sub_races[subRaceSelected][subStatAbbr];
+    } 
     $(`#stat-cards-${group}`).append(
       `
       <div class = "card">
@@ -44,7 +55,7 @@ const statTable = (group, statName, race, statAbbr) => {
       </tr>
       <tr>
       <td>Racial Bonus: </td>
-      <td> ${res[0][statAbbr]} </td>
+      <td> ${res[0][statAbbr] + subRaceStat} </td>
       </tr>
       </tbody>
       </table>
