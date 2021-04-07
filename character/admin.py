b7487d82e0aa4e1ccaa5256bfa4f94171d8898da
Sean Oyler <xrysen@gmail.com>
2021-04-06 18:47:45 -0700

    Adds types, category and properties for weapons

--- character/admin.py
+++ character/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Race, SubRace, Class, Skill, Tool, Alignment, PersonalityTrait, Ideal, Bond, Background, Flaw
+from .models import Race, SubRace, Class, Skill, Tool, Alignment, PersonalityTrait, Ideal, Bond, Background, Flaw, Weapon, WeaponCategory, WeaponProperty, WeaponType
 
 admin.site.register(Race)
 admin.site.register(SubRace)
@@ -14,3 +14,7 @@ admin.site.register(PersonalityTrait)
 admin.site.register(Ideal)
 admin.site.register(Bond)
 admin.site.register(Flaw)
+admin.site.register(Weapon)
+admin.site.register(WeaponCategory)
+admin.site.register(WeaponProperty)
+admin.site.register(WeaponType)
